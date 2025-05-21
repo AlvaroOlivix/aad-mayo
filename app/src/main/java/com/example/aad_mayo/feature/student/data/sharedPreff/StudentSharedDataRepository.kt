7235113a4de166db4a7eff0ee0c73e119f04b64f
alvamun <alvaromunozvaquero@gmail.com>
2025-05-21 17:21:43 +0200

    feature: Ahora con sharedPreff se muestra la lista
    La lista es la misma en mock en el main activity

---
+++ app/src/main/java/com/example/aad_mayo/feature/student/data/sharedPreff/StudentSharedDataRepository.kt
@@ -0,0 +1,15 @@
+package com.example.aad_mayo.feature.student.data.sharedPreff
+
+import com.example.aad_mayo.feature.student.domain.Student
+import com.example.aad_mayo.feature.student.domain.StudentRepository
+
+class StudentSharedDataRepository(private val localXml: LocalXmlSharedDataSource) :
+    StudentRepository {
+    override fun saveAll(students: List<Student>) {
+        localXml.saveStudentList(students)
+    }
+
+    override fun getStudentByExp(expedient: String): Student? {
+        return localXml.getStudentByExpedient(expedient)
+    }
+}
